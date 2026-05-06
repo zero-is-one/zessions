a3fb8f9e4bcbe1aba8ac247e88be653bb1df9b70
Luke NB <luke@0is1.com>
2026-05-06 17:02:50 -0400

    Fix site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  site: "https://zeroisone.github.io",
+  site: "https://zero-is-one.github.io",
   base: isProd ? "/zessions" : "/",
   output: "static",
   integrations: [react()],
